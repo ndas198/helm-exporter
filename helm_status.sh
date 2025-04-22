1cf09b5496bf904c979cf571ba91e58a279823ed
Naba Das <ndas@sticky.io>
2025-04-22 23:11:20 +0530

    add a chat value

--- helm_status.sh
+++ helm_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-helm list --all-namespaces --output json | jq -r '.[] | [.namespace, .name, .status] | @tsv' | while IFS=$'\t' read -r namespace release status; do
+helm list --all-namespaces --output json | jq -r '.[] | [.namespace, .name, .status, .revision, .app_version, (.chart | split("-") | .[-1])] | @tsv' | while IFS=$'\t' read -r namespace release status; do
     metric_name="helm_release_status"
     labels="namespace=\"$namespace\",release=\"$release\",status=\"$status\""
     if [[ "$status" == "failed" ]]; then
